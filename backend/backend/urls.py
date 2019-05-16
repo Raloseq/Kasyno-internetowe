60597b9623a537945304d9357aec02342f314599
Sadaffi <bartek_sadowski@o2.pl>
2019-05-16 21:34:11 +0200

    Django backend setup

---
+++ backend/backend/urls.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('', never_cache(TemplateView.as_view(template_name='index.html')), name='index'),
+    path('admin/', admin.site.urls),
+]
